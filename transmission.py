c8b86513364ed6617d1931c73b11c3676a6b3ea8
Yu Heng Tao <hank.tao@research.uwa.edu.au>
2022-07-29 16:50:20 +0800

    adds maximum detectable absorption to help locate the start of noise floor

--- transmission.py
+++ transmission.py
@@ -3,7 +3,7 @@ Computes complex refractive index of sample given the sample and the reference t
 Created by Yu Heng Tao 29 Nov 2021
 '''
 
-from turtle import forward
+from cProfile import label
 import pandas as pd
 import numpy as np
 np.seterr(divide='ignore', invalid='ignore') #ignore divide by zero warning
@@ -42,18 +42,43 @@ def create_signal(df, window=1):
     f = rfftfreq(len(xt), dT)
     
     amplitude = abs(xf)
-    phase = np.unwrap(np.angle(xf))
+    angle = np.angle(xf)
+
+    phase = np.unwrap(angle)
+
     # shift all phase below zero
     ind = int(1*10**12 / f[1])
+
     gradient = (phase[ind+1]-phase[ind]) / (f[ind+1]-f[ind])
     y_int = phase[ind] - (gradient * f[ind])
     phase -= y_int
 
     return Signal(amplitude[1:], phase[1:], f[1:])
 
-def calc_power(signal):
+def calc_power(signal, max):
     '''returns the power spectrum in dB as an array'''
-    return 20*np.log10(signal.amplitude/signal.amplitude.max())
+    return 20*np.log10(signal.amplitude/max)
+
+
+def find_ind(array, threshold):
+    '''returns the index at which the threshold is first reached'''
+    return np.where(array>=threshold)[0][0]
+
+def calc_DR(signal):
+    ''' returns the freq-domain dynamic range as an array'''
+    # locate the start of noise floor
+    # noise_ind_start = np.where(signal.frequency>=6*10**12)[0][0]
+    noise_ind_start = find_ind(signal.frequency, 6*10**12)
+    # locate the end of noise floor as some systems may have artefacts at very high frequencies
+    try:
+        # noise_ind_end = np.where(signal.frequency>15*10**12)[0][0] 
+        noise_ind_end = find_ind(signal.frequency, 15*10**12)
+    except IndexError:
+        noise_floor = np.mean(signal.amplitude[noise_ind_start:]) # if system has less than 15 THz assume no artefacts at high frequencies and use til end
+        return signal.amplitude/noise_floor
+
+    noise_floor = np.mean(signal.amplitude[noise_ind_start:noise_ind_end])
+    return signal.amplitude/noise_floor
 
 def calc_t_meas(sample, reference):
     '''returns t_meas'''
@@ -153,6 +178,9 @@ def set_skip_sep(system_name):
     if system_name == 'teraview':
         skip = 3
         sep = ','
+    elif system_name == 'toptica':
+        skip = 1
+        sep = ','
     elif system_name == 'menlo':
         skip = 7
         sep = '\t'
@@ -169,17 +197,18 @@ if __name__ == '__main__':
 
     sample_filename, reference_filename = get_filenames()
     tk.Radiobutton(root, text='Teraview', variable=var, value='teraview').grid(row=0, column=0)
-    tk.Radiobutton(root, text='Menlo', variable=var, value='menlo').grid(row=0, column=1)
+    tk.Radiobutton(root, text='TOPTICA', variable=var, value='toptica').grid(row=0, column=1)
+    tk.Radiobutton(root, text='Menlo', variable=var, value='menlo').grid(row=1, column=0)
     
-    tk.Label(root, text='Sample thickness (um):  ').grid(row=1, column=0)
+    tk.Label(root, text='Sample thickness (um):  ').grid(row=2, column=0)
     thickness_entry = tk.Entry(root)
-    thickness_entry.grid(row=1, column=1)
+    thickness_entry.grid(row=2, column=1)
     
-    tk.Label(root, text='Reference refractive index:  ').grid(row=2, column=0)
+    tk.Label(root, text='Reference refractive index:  ').grid(row=3, column=0)
     reference_n_entry = tk.Entry(root)
-    reference_n_entry.grid(row=2, column=1)
+    reference_n_entry.grid(row=3, column=1)
 
-    tk.Button(root, text='Calculate', command=get_user_input).grid(row=3, column=1)
+    tk.Button(root, text='Calculate', command=get_user_input).grid(row=4, column=1)
     root.mainloop()
     root.withdraw()
     
@@ -212,7 +241,7 @@ if __name__ == '__main__':
     plt.show()
 
     # ask user for alternative FP peak location
-    second_peak_ps_sample_corrected = input('Enter alternative FP peak location (ps):  ')
+    second_peak_ps_sample_corrected = input('Input alternative FP peak location (ps) or press Enter if location found was acceptable:  ')
     if second_peak_ps_sample_corrected != '':
         second_peak_ps_sample = float(second_peak_ps_sample_corrected)
     
@@ -220,73 +249,30 @@ if __name__ == '__main__':
     ref_window = create_window(df_sample, second_peak_ps_sample, offset=main_peak_delay_ps)
     
     # calculate optical parameters
-
     sample = create_signal(df_sample, window=sample_window)
     reference = create_signal(df_reference, window=ref_window)  
     reference.n = reference_n
-    
-    auto_thickness = c*10**-6/1.81105*(second_peak_ps_sample - main_peak_ps_sample)/2
-    use_auto_thickness = input(f'Estimated thicknes of ice is {auto_thickness} um; accept [y]/n ?:  ')
-    if use_auto_thickness != 'n':
-        sample.thickness = auto_thickness * 10**-6
-    else:
-        sample.thickness = sample_thickness
+    sample.thickness = sample_thickness
+
+    # estimate thickness with known estimate of average refractive index. For ice n~=1.81105
+    # auto_thickness = c*10**-6/1.81105*(second_peak_ps_sample - main_peak_ps_sample)/2
+    # use_auto_thickness = input(f'Estimated thicknes of ice is {auto_thickness} um; accept [y]/n ?:  ')
+    # if use_auto_thickness != 'n':
+    #     sample.thickness = auto_thickness * 10**-6
     
     # calculate optical parameters
     sample.n, sample.k = calc_complex_n(sample, reference)
     sample.absorption = 2*(2*np.pi*sample.frequency)/c*sample.k/100
     sample.permittivity = (sample.n**2 - sample.k**2) - 1j*2*sample.n*sample.k
-    
-    # explore oscillation in t_meas due to FP
-    # A, phi = calc_t_meas(sample, reference)
-
-    ## Fourier transform t_meas to find freq of oscillation
-    # xt = A_raw[5:100] - A[5:100]
-    # xf_raw = rfft(xt)
-    # dT = sample.frequency[2] - sample.frequency[1]
-    # f = rfftfreq(len(xt), dT)
-
-    # fig, axs = plt.subplots(2)
-    # axs[0].plot(sample.frequency[5:100]*10**-12, xt, label='raw')
-    # axs[0].set_ylabel('modulus of t_meas')
-    # axs[0].legend()
-    # axs[1].plot(f*c/2/sample.n[30]*10**6, abs(xf_raw), label='diff')
-    # axs[1].set_yscale('log')
-    # axs[1].set_ylabel('FFT of modulus of t_meas')
-    # axs[1].legend()
-    # plt.show()
-
-    ## calc FP using best guess of n-jk and divide t_meas by FP to remove it
-    # FP = calc_FP(sample, reference, p=1)
-    # A_div_FP = A_raw / np.absolute(FP)
-
-    # plt.plot(sample.frequency*10**-12, A_raw, label='raw')
-    # plt.plot(sample.frequency*10**-12, A, label='filtered')
-    # plt.plot(sample.frequency*10**-12, A_div_FP, label='div FP')
-    # plt.xlim([0,5])
-    # plt.xlabel('t_meas (a.u.)')
-    # plt.ylim([0, 2.5])
-    # plt.ylabel('Frequency (THz)')
-    # plt.legend()
-    # plt.show()
-    
+     
     # calculate power spectra
-    sample.power = calc_power(sample)
-    reference.power = calc_power(reference)
+    max = reference.amplitude.max()
+    sample.power = calc_power(sample, max)
+    reference.power = calc_power(reference, max)
     
-    # fig, axs = plt.subplots(2, sharex=True)
-    # axs[0].plot(sample.frequency*10**-12, sample.amplitude, label='sample')
-    # axs[0].plot(reference.frequency*10**-12, reference.amplitude, label='reference')
-    # axs[1].plot(sample.frequency*10**-12, sample.phase, label='sample')
-
-    # axs[1].plot(reference.frequency*10**-12, reference.phase, label='reference')
-    # axs[0].set_xlim([0, 5])
-    # axs[0].set_ylabel('FFT amplitude')
-    # axs[1].set_xlabel('Frequency (THz)')
-    # axs[1].set_ylabel('FFT phase')
-    # axs[0].legend()
-    # axs[1].legend()
-    # plt.show()
+    # calculate maximum absorption
+    DR = calc_DR(reference)
+    a_max = 2*np.log(DR*4*sample.n/(sample.n+1)**2)/(sample.thickness*100)
 
     # plots for display
     fig, axs = plt.subplots(2, 2)
@@ -306,19 +292,14 @@ if __name__ == '__main__':
     axs[1,0].set_ylabel('k')
     axs[1,0].set_ylim([-0.1,0.4])
 
-    # axs[0,1].plot(sample.frequency*10**-12, sample.amplitude, label='sample')
-    # axs[0,1].plot(reference.frequency*10**-12, reference.amplitude, label='reference')
-    # axs[0,1].set_xlim([0.2, 5])
-    # axs[0,1].set_xlabel('Frequency (THz)')
-    # axs[0,1].set_ylabel('Amplitude')
-    # axs[0,1].legend()
-
-    axs[0,1].plot(sample.frequency*10**-12, sample.absorption)
+    axs[0,1].plot(sample.frequency*10**-12, sample.absorption, label='Sample')
+    axs[0,1].plot(sample.frequency*10**-12, a_max, label='Max detectable')
     axs[0,1].set_xticks(freq_range)
     axs[0,1].set_xlim([0.2, 5])
     axs[0,1].set_ylim(bottom=0)
     axs[0,1].set_xlabel('Frequency (THz)')
     axs[0,1].set_ylabel('Absorption coefficient ($cm^{-1}$)')
+    axs[0,1].legend()
 
     axs[1,1].plot(sample.frequency*10**-12, sample.power, label='Sample')
     axs[1,1].plot(reference.frequency*10**-12, reference.power, label='Reference') 
@@ -326,21 +307,13 @@ if __name__ == '__main__':
     axs[1,1].set_xlabel('Frequency (THz)')
     axs[1,1].set_ylabel('Power (dB)')
     axs[1,1].legend()
-
-    # axs[1,1].plot(sample.frequency*10**-12, np.real(sample.permittivity), label='Re[e]')
-    # axs[1,1].plot(sample.frequency*10**-12, np.imag(sample.permittivity), label='Im[e]')
-    # axs[1,1].set_xlim([0.2, 5])
-    # axs[1,1].set_xlabel('Frequency (THz)')
-    # axs[1,1].set_ylabel('Permittivity')
-    # axs[1,1].legend()
-
     
     plt.tight_layout()
     plt.show()
 
     # save results
-    export_array = np.stack((sample.frequency*10**-12, sample.n, sample.absorption, reference.power, sample.power)).T[1:]
-    export_df = pd.DataFrame(export_array, columns=['THz','n','a', 'ref_power', 'sample_power'])
+    export_array = np.stack((sample.frequency*10**-12, sample.n, sample.absorption, a_max, sample.power, reference.power)).T[1:]
+    export_df = pd.DataFrame(export_array, columns=['THz','n','a', 'max detectable', 'sample_power', 'ref_power'])
 
     try:
         with filedialog.asksaveasfile(mode='w', defaultextension=".csv") as file:
